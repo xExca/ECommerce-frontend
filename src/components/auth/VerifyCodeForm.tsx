1d947720c6d79da5ed5a9efa787e739915d51a39
Onins <naustral@ncmh.gov.ph>
2026-01-22 10:09:39 +0800

    Verification route path change

--- src/components/auth/VerifyCodeForm.tsx
+++ src/components/auth/VerifyCodeForm.tsx
@@ -67,7 +67,7 @@ const VerifyCodeForm = ({identifier, payload, setVerifyCode, from, resetLoginFlo
     const email = from === "signup" && payload ? payload.email : identifier;
 
     try {
-      await axios.get(`/api/auth/${from === "signup" ? "resend-otpSignup" : "resend-otp"}/${email}`);
+      await axios.get(`/api/auth/${from === "signup" ? "resend-otpSignup" : "resend"}/${email}`);
     } catch (error) {
       console.error(error);
       setCooldown(0);
